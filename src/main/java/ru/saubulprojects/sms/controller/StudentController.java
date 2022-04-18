7d9d672cf661cf81a8801252b5c9602fe211ff25
Ruslan <miraxle@yandex.ru>
2022-04-18 20:55:33 +0300

    Исправлены ошибки

--- src/main/java/ru/saubulprojects/sms/controller/StudentController.java
+++ src/main/java/ru/saubulprojects/sms/controller/StudentController.java
@@ -5,7 +5,6 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,7 +45,7 @@ public class StudentController {
 	}
 	
 	@PostMapping("/edit/{id}")
-	public String editStudent(@PathVariable("id") Long id, @ModelAttribute("student") Student student, Errors errors) {
+	public String editStudent(@PathVariable("id") Long id, @Valid @ModelAttribute("student") Student student, Errors errors) {
 		if(errors.hasErrors()) {
 			return "student/edit_student";
 		}
